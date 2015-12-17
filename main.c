f5d7f38960bf432c3d721e66212c113d24c65845
Jitbim <jolien_jeunen@hotmail.com>
2015-12-17 15:07:43 +0100

    msg

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int som;
+int a;
+int b;
+int c;
+
+int main(void)
+{
+    printf("Geef 3 getallen\n");
+    scanf("%d %d %d", &a, &b, &c);
+    som = (a + b + c);
+    printf("De som is = %d\n", som);
+    return 0;
+}
+
